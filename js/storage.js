6fa75671e40056978a1f49f68a45bd3d2337181b
Sophia Brouwers <you@example.com>
2024-04-03 20:57:41 +0200

    adjusted storage.js register.js and index.html and added token

--- js/storage.js
+++ js/storage.js
@@ -1,4 +1,4 @@
-const STORAGE_TOKEN = "HIER DEIN TOKEN EINFÜGEN"; // Todo: Token einfügen
+const STORAGE_TOKEN = "39JY5O3SG0HK2DPJ5AWZAZ4NV9422TX3UGN1SNUX";
 const STORAGE_URL = "https://remote-storage.developerakademie.org/item";
 
 async function setItem(key, value) {
@@ -8,7 +8,14 @@ async function setItem(key, value) {
 
 async function getItem(key) {
   const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
-  return fetch(url).then((res) => res.json());
+  return fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.data) {
+        return res.data.value;
+      }
+      throw `Could not find data with key "${key}".`;
+    });
 }
 
 // Hinweis aus dem Video: eine direkte "delete" Funktion gibt es nicht, von daher kannst du deine Daten nur "leeren". Die Lösung ist hier somit ein leeres Array hoch zu laden.
