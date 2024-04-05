8cefbb39e5794239a743966fd17f4858a0dffa37
Alicja K <akwiaton@gmail.com>
2024-04-05 16:26:48 +0200

    Signup & login css, html, js changed

--- sign_up/sign_up.js
+++ sign_up/sign_up.js
@@ -1,9 +1,26 @@
 let users = [];
 
 function addUser() {
+  let name = document.getElementById("name");
   let email = document.getElementById("email");
   let password = document.getElementById("password");
-  users.push({email: email.value, password: password.value});
+  let passwordConfirm = document.getElementById("password_confirm");
+
+  let user = {
+    "name": name.value,
+    "email": email.value,
+    "password": password.value,
+    "passwordConfirm": passwordConfirm.value
+  };
+
+  users.push(user);
+
+  console.log(users);
+
+  name.value = '';
+  email.value = '';
+  password.value = '';
+  passwordConfirm.value = '';
   // Weiterleitung zu Login Seite + Nachricht anzeigen: "Erfolgreiche Registrierung" auf login.html
-  window.location.href = "login.html?msg=Du hast dich erfolgreich registriert";
+  // window.location.href = "login.html?msg=Du hast dich erfolgreich registriert";
 }
