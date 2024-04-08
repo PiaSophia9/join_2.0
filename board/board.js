bd639d544949ef90536b1c6a79af807cb3e8eea6
DonDomenico <d.grunow@hotmail.de>
2024-04-08 18:15:29 +0200

    status of task now gets updated on server when moved to another status-area on the boad

--- board/board.js
+++ board/board.js
@@ -2,29 +2,42 @@ let todos = [];
 
 let currentDraggedElement;
 
-async function init() {
+async function initBoard() {
     await loadAllTasks();
     updateHTML();
 }
 
+/**
+ * This function loads the array "allTasks" from the server and assign it to the array "todos"
+ */
 async function loadAllTasks() {
     let response = await getItem('remoteTasks');
     todos = JSON.parse(response);
-    console.log(todos);
 }
 
+/**
+ * This function updates the task areas. 
+ * The todo-Array is filtered for each status and a new array for the tasks at this specific status is given back.
+ * Then, these arrays are passed into the function "updateArea"
+ */
 function updateHTML() {
-    let todo = todos.filter((t) => t["status"] == "todo");
+    let todo = todos.filter((t) => t["status"] == "toDo");
     let inProgress = todos.filter((t) => t["status"] == "in-progress");
     let awaitFeedback = todos.filter((t) => t["status"] == "await-feedback");
     let done = todos.filter((t) => t["status"] == "done");
 
-    updateArea("todo", todo);
+    updateArea("toDo", todo);
     updateArea("in-progress", inProgress);
     updateArea("await-feedback", awaitFeedback);
     updateArea("done", done);
 }
 
+/**
+ * In this function, the task-area first gets cleared. After that, if the areaArray is empty, the function "generateEmptyHTML" is called. This function just return a div with the text "no tasks here".
+ * If the array isn't empty, the task-element for every task in the array is created by calling the function "generateTodoHTML"
+ * @param {string} areaName 
+ * @param {Array} areaArray 
+ */
 function updateArea(areaName, areaArray) {
     document.getElementById(areaName).innerHTML = "";
     if(areaArray.length == 0) {
@@ -43,8 +56,9 @@ function startDragging(id) {
 
 function generateTodoHTML(element) {
     return /*html*/ `
-        <div draggable="true" ondragstart="startDragging(${element["category"]})" class="todo">
-            <p>${element["title"]}</p>
+        <div draggable="true" ondragstart="startDragging(${todos.indexOf(element)})" class="todo">
+            <p>${element["category"]}</p>
+            <h2>${element["title"]}</h2>
         </div>
     `;
 }
@@ -57,11 +71,15 @@ function allowDrop(event) {
     event.preventDefault();
 }
 
-function moveTo(category) {
-    todos[currentDraggedElement]["category"] = category;
-    //ZB Todo mit id 1: Das Feld 'category' ändert sich zu open oder closed
+async function moveTo(status) {
+    todos[currentDraggedElement]["status"] = status;
+    // update status in database
+    await storeAllTasks();
+    // load tasks from database
+    await loadAllTasks();
     updateHTML();
 }
+
 
 function highlight(id) {
     document.getElementById(id).classList.add("drag-area-highlight");
@@ -69,4 +87,8 @@ function highlight(id) {
 
 function removeHighlight(id) {
     document.getElementById(id).classList.remove("drag-area-highlight");
+}
+
+async function storeAllTasks() {
+    await setItem("remoteTasks", todos);
 }
