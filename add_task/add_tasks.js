2d335cc190247e7c1e8e8e18ff5e591f0e9d892b
Sophia Brouwers <you@example.com>
2024-04-11 14:33:15 +0200

    probably no changes

--- add_task/add_tasks.js
+++ add_task/add_tasks.js
@@ -164,12 +164,22 @@ let contactColors = [];
 //     assignedContactColors: assignedContactColors
 //   }
 //   ]
+let subtasks [
+  {
+nameSubtask: nameSubtask,
+stausSubtask:
+},
+{
+  nameSubtask:
+  stausSubtask:
+  },
+]
 
 let assignedContacts = [];
 
 // let assignedContacts = [];
-// let assignedContactInitials = [];
-// let assignedContactColors = [];
+let assignedContactInitials = [];
+let assignedContactColors = [];
 
 let colors = ["#FF7A00", "#FF5EB3", "#6E52FF", "#9327FF", "#00BEE8", "#1FD7C1", "#FF745E", "#FFA35E", "#FC71FF", "#FFC701", "#0038FF", "#C3FF2B", "#FFE62B", "#FF4646", "#FFBB2B"];
 
