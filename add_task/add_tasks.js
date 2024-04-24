2da0f67447588bc95a8ea16d0aed75da2deba54e
Sophia Brouwers <you@example.com>
2024-04-24 14:09:40 +0200

    added outcommented function for later

--- add_task/add_tasks.js
+++ add_task/add_tasks.js
@@ -16,6 +16,12 @@ async function init() {
   console.log("allTasks", allTasks);
 }
 
+// input.addEventListener("keypress", function (event) {
+//   if (event.keyCode == 13) {
+//     addSubtask();
+//   }
+// });
+
 /**
  * This function loads an array from the storage and parses it.
  *
