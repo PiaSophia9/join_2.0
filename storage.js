7e2e59ee3678080da1f4377640c11ab70e6f88cc
Alicja K <akwiaton@gmail.com>
2024-04-25 14:06:10 +0200

    header.js jdocs added

--- storage.js
+++ storage.js
@@ -1,12 +1,24 @@
 const STORAGE_TOKEN = "39JY5O3SG0HK2DPJ5AWZAZ4NV9422TX3UGN1SNUX";
 const STORAGE_URL = "https://remote-storage.developerakademie.org/item";
 
+/**
+ * Sets an item in the storage with the given key and value.
+ *
+ * @param {string} key - The key of the item to be set.
+ * @param {any} value - The value of the item to be set.
+ * @return {Promise<any>} A promise that resolves to the response from the server.
+ */
 async function setItem(key, value) {
   const payload = {key, value, token: STORAGE_TOKEN};
   return fetch(STORAGE_URL, {method: "POST", body: JSON.stringify(payload)}).then((res) => res.json());
-  // .then((res) => res.json()) Braucht man das?
 }
 
+/**
+ * Retrieves an item from the storage with the given key.
+ *
+ * @param {string} key - The key of the item to retrieve.
+ * @return {Promise<any>} A promise that resolves to the value of the item, or throws an error if the item is not found.
+ */
 async function getItem(key) {
   const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
   return fetch(url)
